ba134bd985d889d15a61affaade76ee7445dd199
endor <fp@notjusthosting.com>
2012-05-19 16:23:58 +0200

    test extras and quantity

---
+++ test/acceptance/extras.js
@@ -0,0 +1,22 @@
+casper.start('http://localhost:3000/', function() {
+    this.click('[alt="chocolate chips"]');
+});
+
+casper.then(function() {
+    this.test.assertTextExists('You are about to order 1 Banana bread loaf with chocolate chips.');
+    this.test.assert(this.evaluate(function() { return $("[alt=\"chocolate chips\"]").hasClass("selected"); }));
+    this.click('.order');
+});
+
+casper.then(function() {
+    this.test.assertTextExists('You are about to order 1 Banana bread loaf with chocolate chips.');
+    this.click('.order');
+});
+
+casper.then(function() {
+    this.test.assertTextExists('You are about to order 1 Banana bread loaf with chocolate chips.');
+});
+
+casper.run(function() {
+    this.test.done();
+});
--- test/acceptance/quantity.js
+++ test/acceptance/quantity.js
@@ -9,6 +9,20 @@ casper.then(function() {
 
 casper.then(function() {
     this.test.assertTextExists('You are about to order 1 Banana bread loaf.');
+    this.click('#quantity-plus');
+});
+
+casper.then(function() {
+    this.click('.order');
+});
+
+casper.then(function() {
+    this.test.assertTextExists('You are about to order 2 Banana bread loaves.');
+    this.click('.order');
+});
+
+casper.then(function() {
+    this.test.assertTextExists('You are about to order 2 Banana bread loaves.');
 });
 
 casper.run(function() {
