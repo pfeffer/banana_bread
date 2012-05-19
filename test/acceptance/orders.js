623330b7ae926b1335d47a048d906c315d0bcc78
endor <fp@notjusthosting.com>
2012-05-19 15:49:09 +0200

    add a first basic casperjs acceptance test

---
+++ test/acceptance/orders.js
@@ -0,0 +1,22 @@
+var casper = require('casper').create();
+
+casper.start('http://localhost:3000/', function() {
+    this.click('.order');
+});
+
+casper.then(function() {
+    this.click('.order');
+});
+
+casper.then(function() {
+   this.test.assertTextExists('You chose pickup. Order number is');
+   this.click('[value="Pay"]');
+});
+
+casper.then(function() {
+   this.test.assertUrlMatch(/www\.sandbox\.paypal\.com/); 
+});
+
+casper.run(function() {
+    this.test.renderResults(true);
+});
